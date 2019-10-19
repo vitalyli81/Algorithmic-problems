105b1de7cd5ab6289a5c7839046000bb4d545ad6
Vitaly Li <>
2019-10-19 11:27:14 -0400

    auto complete

---
+++ src/problems/designSearchAutocompleteSystem/AutocompleteSystem.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} sentences
+ * @param {number[]} times
+ */
+class AutocompleteSystem {
+  constructor(sentences, times) {
+    // TODO
+  }
+
+  /**
+   * @param {character} c
+   * @return {string[]}
+   */
+  input(c) {
+    // TODO
+  }
+}
+
+/**
+ * Your AutocompleteSystem object will be instantiated and called as such:
+ * var obj = new AutocompleteSystem(sentences, times)
+ * var param_1 = obj.input(c)
+ */
