517d1dbe2b8249bbcadc34501f51ac1dd773d5b4
Vitaly Li <>
2019-11-03 15:58:27 -0500

    count smaller

--- src/problems/countofSmallerNumbersAfterSelf/countSmaller2.js
+++ src/problems/countofSmallerNumbersAfterSelf/countSmaller2.js
@@ -1,50 +1,41 @@
 export const countSmaller = nums => {
   const result = Array(nums.length);
-  const bst = new Bst();
+  const bst = new Node(nums[nums.length - 1]);
+  result[result.length - 1] = 0;
 
-  for (let i = nums.length - 1; i >= 0; i--) {
-    result[i] = bst.add(nums[i]);
+  for (let i = nums.length - 2; i >= 0; i--) {
+    result[i] = bst.add(nums[i], 0);
   }
 
   return result;
 };
 
-class Bst {
-  constructor() {
-    this.root = null;
+class Node {
+  constructor(val, size = 0) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+    this.size = size;
   }
 
-  add(key) {
-    let node = this.root;
-    let leaf = node;
-    const newNode = {
-      value: key,
-      count: 0
-    };
-
-    while (node) {
-      leaf = node;
-
-      if (key > node.value) {
-        newNode.count++;
-        node = node.right;
+  add(val, size) {
+    if (val > this.val) {
+      if (this.right) {
+        return this.right.add(val, this.size + size + 1);
       } else {
-        node = node.left;
+        this.right = new Node(val);
+        return this.size + size + 1;
+      }
+    } else {
+      this.size++;
+
+      if (this.left) {
+        return this.left.add(val, size);
+      } else {
+        this.left = new Node(val);
+        return size;
       }
     }
-
-    if (!this.root) {
-      this.root = newNode;
-      return 0;
-    }
-
-    if (key > leaf.value) {
-      leaf.right = newNode;
-    } else {
-      leaf.left = newNode;
-    }
-
-    return newNode.count;
   }
 }
 
