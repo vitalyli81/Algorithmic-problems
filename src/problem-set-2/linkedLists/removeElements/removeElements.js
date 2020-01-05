9541ac7a85d7b778d89229d0080dce8b6395e6ce
Vitaly Li <>
2020-01-04 22:14:27 -0500

    remove els

---
+++ src/problem-set-2/linkedLists/removeElements/removeElements.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  if (!head) return head;
+  let dummy = new ListNode(0);
+  dummy.next = head;
+  let current = head;
+  let prev = dummy;
+
+  while (current) {
+    if (current.val === val) {
+      prev.next = current.next;
+    } else {
+      prev = current;
+    }
+    current = current.next;
+  }
+
+  return dummy.next;
+};
