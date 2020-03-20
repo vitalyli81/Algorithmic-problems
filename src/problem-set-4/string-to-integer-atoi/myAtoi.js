c0623b77ad57a048666c90adb48e56d070b1bfb1
Vitaly Li <>
2020-03-20 12:25:26 -0400

    string to integer

--- src/problem-set-4/string-to-integer-atoi/myAtoi.js
+++ src/problem-set-4/string-to-integer-atoi/myAtoi.js
@@ -2,4 +2,32 @@
  * @param {string} str
  * @return {number}
  */
-var myAtoi = function(str) {};
+var myAtoi = function(str) {
+  let res = 0;
+  let i = 0;
+  let isNegative = false;
+
+  while (str[i] === ' ') {
+    i++;
+  }
+
+  if (str[i] === '+' || str[i] === '-') {
+    isNegative = str[i] === '-';
+    i++;
+  }
+
+  for (; i < str.length; i++) {
+    const code = str.charCodeAt(i) - 48;
+    if (code < 0 || code > 9) {
+      break;
+    }
+    res *= 10;
+    res += code;
+  }
+
+  if (isNegative) {
+    res = -res;
+  }
+
+  return Math.max(-(2 ** 31), Math.min(res, 2 ** 31 - 1));
+};
