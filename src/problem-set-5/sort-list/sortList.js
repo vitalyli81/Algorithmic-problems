4ac6d8138053e3dbb34b5517ace67b6b1b071898
Vitaly Li <>
2020-04-21 20:32:51 -0400

    sortList

---
+++ src/problem-set-5/sort-list/sortList.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function (head) {
+  if (!head || !head.next) return head;
+  let slow = head;
+  let fast = head.next;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  fast = slow.next;
+  slow.next = null;
+
+  return mergeLists(sortList(head), sortList(fast));
+};
+
+function mergeLists(list1, list2) {
+  const tempHead = {};
+  let cur = tempHead;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      cur.next = list1;
+      list1 = list1.next;
+    } else {
+      cur.next = list2;
+      list2 = list2.next;
+    }
+
+    cur = cur.next;
+  }
+
+  return tempHead.next;
+}
